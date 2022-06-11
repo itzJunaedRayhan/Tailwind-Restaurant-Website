164b0fb6d6f08c9db0414e03bc6c1426ac4c705a
itzJunaedRayhan <webgenesis99@gmail.com>
2022-06-11 22:51:07 +0600

    About Section Done

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,7 @@ let toggleIcon = document.getElementById('toggle-icon');
 let fullBodyWrapper = document.getElementById('full-wrapper');
 
 let productWrapper = document.getElementById('products-wrapper');
+var mobileDeviceNav = document.getElementsByClassName("navbar-wrapper");
 let product = productWrapper.getElementsByClassName("product")
 let left = document.querySelector('.left');
 let right = document.querySelector('.right');
@@ -10,6 +11,7 @@ let right = document.querySelector('.right');
 //  Display Navigation on click event:
 toggleIcon.addEventListener('click', () => {
     fullBodyWrapper.classList.toggle('display-navigation');
+    mobileDeviceNav[0].classList.toggle('control-nav');
 });
 
 
@@ -25,3 +27,16 @@ left.addEventListener('click', () => {
     console.log("Hello");
     productWrapper.prepend(product[product.length - 1])
 })
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+fullBodyWrapper.onscroll = function () { 
+    if (fullBodyWrapper.scrollTop >= 100 ) {
+        mobileDeviceNav[0].classList.add("nav-bg");
+    }else{
+        mobileDeviceNav[0].classList.remove("nav-bg");
+    } 
+    
+};
